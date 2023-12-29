c9fbe069761e81d5815d045126877a45f9da2792
Your Name <you@example.com>
2023-12-29 17:11:35 +0300

    Add project files.

---
+++ Labw5/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labw5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
+public partial class MainWindow : Window
+{
+    public ObservableCollection<Task> Tasks { get; set; }
+    public Task SelectedTask { get; set; }
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        Tasks = new ObservableCollection<Task>();
+        taskListView.ItemsSource = Tasks;
+        DataContext = this;
+    }
+
+    private void AddTask_Click(object sender, RoutedEventArgs e)
+    {
+        // Реализуйте логику добавления задачи
+    }
+
+    private void EditTask_Click(object sender, RoutedEventArgs e)
+    {
+        // Реализуйте логику редактирования задачи
+    }
+
+    private void DeleteTask_Click(object sender, RoutedEventArgs e)
+    {
+        // Реализуйте логику удаления задачи
+    }
+}
---
+++ Labw5/Task.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labw5
+{
+    internal class Task
+    {
+    }
+}
+public class Task : INotifyPropertyChanged
+{
+    private string title;
+    private string description;
+    private DateTime deadline;
+
+    public string Title
+    {
+        get { return title; }
+        set
+        {
+            title = value;
+            OnPropertyChanged(nameof(Title));
+        }
+    }
+
+    public string Description
+    {
+        get { return description; }
+        set
+        {
+            description = value;
+            OnPropertyChanged(nameof(Description));
+        }
+    }
+
+    public DateTime Deadline
+    {
+        get { return deadline; }
+        set
+        {
+            deadline = value;
+            OnPropertyChanged(nameof(Deadline));
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
